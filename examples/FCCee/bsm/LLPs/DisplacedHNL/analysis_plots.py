d5429d8d61e37127a5aa71ec1c793fab45101fca
el-doardo <edoardospoto96.es@gmail.com>
2026-01-22 18:02:10 +0100

    Update analysis_plots.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_plots.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_plots.py
@@ -1,7 +1,7 @@
 import ROOT
 
 # global parameters
-intLumi        = 180.0e+06 #in pb-1
+intLumi        = 150.0e+06 #in pb-1
 
 ###If scaleSig=0 or scaleBack=0, we don't apply any additional scaling, on top of the normalization to cross section and integrated luminosity, as defined in finalSel.py
 ###If scaleSig or scaleBack is not defined, plots will be normalized to 1
@@ -11,9 +11,9 @@ ana_tex        = "e^{+}e^{-} #rightarrow N_{1,2} #nu, N_{1,2} #rightarrow ll#nu"
 delphesVersion = '3.4.2'
 energy         = 91
 collider       = 'FCC-ee'
-inputDir       = '/eos/user/s/sgiappic/2HNL_ana/final/'
-outdir         = '/eos/user/s/sgiappic/2HNL_ana/plots/'
-formats        = ['png', 'pdf']
+inputDir       = '/eos/user/e/espoto/FCCAnalyses/outputs/gen/final_gen2/'
+outdir         = '/eos/user/e/espoto/FCCAnalyses/outputs/gen/plots_gen2/'
+formats        = ['png']
 #formats        = ['pdf']
 #yaxis          = ['lin','log']
 yaxis          = ['log']
@@ -26,20 +26,22 @@ splitLeg       = True ### to split legend for backgrounds and signals ###
 variables = [
 
     #gen variables
+        "FSGenMissing_e", ## ## definito da me in stage1
+	"FSGenMissing_pt", ## ## definito da me in stage1
     "n_FSGenElectron",
     "n_FSGenMuon",
     "n_FSGenLepton",
     "n_GenN",
-    "n_FSGenPhoton",
-    #"n_FSGenNeutrino",
-    
-    #"FSGenElectron_e",
+    #"n_FSGenPhoton",
+    "n_FSGenNeutrino",
+
+    "FSGenElectron_e",     ## ## just a note: following two group of raws are defined in stage1, but no hint in final
     #"FSGenElectron_p",
-    #"FSGenElectron_pt",
+    "FSGenElectron_pt",
     #"FSGenElectron_pz",
-    #"FSGenElectron_eta",
+    "FSGenElectron_eta",
     #"FSGenElectron_theta",
-    #"FSGenElectron_phi",
+    "FSGenElectron_phi",
     #"FSGenElectron_vertex_x",
     #"FSGenElectron_vertex_y",
     #"FSGenElectron_vertex_z",
@@ -47,13 +49,13 @@ variables = [
     #"FSGenElectron_vertex_y_prompt",
     #"FSGenElectron_vertex_z_prompt",
 
-    #"FSGenMuon_e",
+    "FSGenMuon_e",
     #"FSGenMuon_p",
-    #"FSGenMuon_pt",
+    "FSGenMuon_pt",
     #"FSGenMuon_pz",
-    #"FSGenMuon_eta",
+    "FSGenMuon_eta",
     #"FSGenMuon_theta",
-    #"FSGenMuon_phi",
+    "FSGenMuon_phi",
     #"FSGenMuon_vertex_x",
     #"FSGenMuon_vertex_y",
     #"FSGenMuon_vertex_z",
@@ -61,13 +63,27 @@ variables = [
     #"FSGenMuon_vertex_y_prompt",
     #"FSGenMuon_vertex_z_prompt",
 
-    #"FSGenNeutrino_e",
+    "FSGenLepton_e",   
+    #"FSGenLepton_p",
+    "FSGenLepton_pt",
+    #"FSGenLepton_pz",
+    "FSGenLepton_eta",
+    #"FSGenLepton_theta",
+    "FSGenLepton_phi",
+    #"FSGenLepton_vertex_x",
+    #"FSGenLepton_vertex_y",
+    #"FSGenLepton_vertex_z",
+    #"FSGenLepton_vertex_x_prompt",
+    #"FSGenLepton_vertex_y_prompt",
+    #"FSGenLepton_vertex_z_prompt",
+
+    "FSGenNeutrino_e",
     #"FSGenNeutrino_p",
-    #"FSGenNeutrino_pt",
+    "FSGenNeutrino_pt",
     #"FSGenNeutrino_pz",
-    #"FSGenNeutrino_eta",
+    "FSGenNeutrino_eta",
     #"FSGenNeutrino_theta",
-    #"FSGenNeutrino_phi",
+    "FSGenNeutrino_phi",
 
     #"FSGenPhoton_e",
     #"FSGenPhoton_p",
@@ -79,25 +95,28 @@ variables = [
 
     "FSGen_Lxy",
     "FSGen_Lxyz",
-    "FSGen_Lxyz_prompt",
+    "GenN_Lxyz",
+    ## ##"FSGen_Lxyz_prompt",
     "FSGen_invMass",
 
     "GenN_mass",
-    "GenN_time",
-    "GenN_tau",
-    "GenN_txyz",
-    "GenN_Lxyz_tau",
-    "GenN_Lxyz_time",
+    "GenN_e",
+    "GenN_p",
+    ## ## "GenN_time", ## ## no hint of it in final nor stage1
+    #"GenN_tau",
+    ## ## "GenN_txyz", ## ## no hint of it in final nor stage1
+    ## ## "GenN_Lxyz_tau", ## ## no hint of it in final nor stage1
+    ## ## "GenN_Lxyz_time",# # ## no hint of it in final nor stage1
 
     #reco variables
-    "n_RecoTracks",
-    "n_PrimaryTracks",
-    "n_SecondaryTracks",
-    "n_RecoJets",
-    "n_RecoPhotons",
-    "n_RecoElectrons",
-    "n_RecoMuons",
-    "n_RecoLeptons",
+    ## ##"n_RecoTracks",
+    ## ##"n_PrimaryTracks",
+    ## ##"n_SecondaryTracks",
+    ## ##"n_RecoJets",
+    ## ##"n_RecoPhotons",
+    ## ##"n_RecoElectrons",
+    ## ##"n_RecoMuons",
+    ## ##"n_RecoLeptons",
 
     #"jets_e",
     #"RecoJet_e",
@@ -119,18 +138,18 @@ variables = [
     #"RecoJetTrack_D0cov",
     #"RecoJetTrack_Z0cov",
 
-    #"RecoElectron_e",
-    #"RecoElectron_p",
-    #"RecoElectron_pt",
-    #"RecoElectron_px",
-    #"RecoElectron_py",
-    #"RecoElectron_pz",
-    #"RecoElectron_eta",
-    #"RecoElectron_theta",
-    #"RecoElectron_phi",
+    ## ##"RecoElectron_e",
+    ## ##"RecoElectron_p",
+    ## ##"RecoElectron_pt",
+    ## ##"RecoElectron_px",
+    ## ##"RecoElectron_py",
+    ## ##"RecoElectron_pz",
+    ## ##"RecoElectron_eta",
+    ## ##"RecoElectron_theta",
+    ## ##"RecoElectron_phi",
 
-    #"RecoElectronTrack_absD0",
-    #"RecoElectronTrack_absD0_prompt",
+    ## ##"RecoElectronTrack_absD0",
+    ## ##"RecoElectronTrack_absD0_prompt",
     #"RecoElectronTrack_absZ0",
     #"RecoElectronTrack_absZ0_prompt",
     #"RecoElectronTrack_absD0sig",
@@ -141,58 +160,58 @@ variables = [
     #"RecoElectronTrack_D0cov",
     #"RecoElectronTrack_Z0cov",
 
-    "Reco_e",
-    "Reco_p",
-    "Reco_pt",
-    "Reco_px",
-    "Reco_py",
-    "Reco_pz",
-    "Reco_eta",
-    "Reco_theta",
-    "Reco_phi",
+    ## ##"Reco_e",
+    ## ##"Reco_p",
+    ## ##"Reco_pt",
+    ## ##"Reco_px",
+    ## ##"Reco_py",
+    ## ##"Reco_pz",
+    ## ##"Reco_eta",
+    ## ##"Reco_theta",
+    ## ##"Reco_phi",
 
-    "RecoTrack_absD0_prompt",
-    "RecoTrack_absZ0_prompt",
-    "RecoTrack_absD0_med",
-    "RecoTrack_absZ0_med",
-    "RecoTrack_absD0",
-    "RecoTrack_absZ0",
-    "RecoTrack_absD0sig",
-    "RecoTrack_absD0sig_med",
-    "RecoTrack_absD0sig_prompt",
-    "RecoTrack_absZ0sig",
-    "RecoTrack_absZ0sig_med",
-    "RecoTrack_absZ0sig_prompt",
-    "RecoTrack_D0cov",
-    "RecoTrack_Z0cov",
+    ## ##"RecoTrack_absD0_prompt",
+    ## ##"RecoTrack_absZ0_prompt",
+    ## ##"RecoTrack_absD0_med",
+    ## ##"RecoTrack_absZ0_med",
+    ## ##"RecoTrack_absD0",
+    ## ##"RecoTrack_absZ0",
+    ## ##"RecoTrack_absD0sig",
+    ## ##"RecoTrack_absD0sig_med",
+    ## ##"RecoTrack_absD0sig_prompt",
+    ## ##"RecoTrack_absZ0sig",
+    ## ##"RecoTrack_absZ0sig_med",
+    ## ##"RecoTrack_absZ0sig_prompt",
+    ## ##"RecoTrack_D0cov",
+    ## ##"RecoTrack_Z0cov",
 
-    "Reco_DecayVertexLepton_x",       
-    "Reco_DecayVertexLepton_y",          
-    "Reco_DecayVertexLepton_z",          
-    "Reco_DecayVertexLepton_x_prompt",   
-    "Reco_DecayVertexLepton_y_prompt",    
-    "Reco_DecayVertexLepton_z_prompt",    
-    "Reco_DecayVertexLepton_chi2",    
-    "Reco_DecayVertexLepton_probability", 
+    ## ##"Reco_DecayVertexLepton_x",       
+    ## ##"Reco_DecayVertexLepton_y",          
+    ## ##"Reco_DecayVertexLepton_z",          
+    ## ##"Reco_DecayVertexLepton_x_prompt",   
+    ## ##"Reco_DecayVertexLepton_y_prompt",    
+    ## ##"Reco_DecayVertexLepton_z_prompt",    
+    ## ##"Reco_DecayVertexLepton_chi2",    
+    ## ##"Reco_DecayVertexLepton_probability", 
 
-    "Reco_Lxy",
-    "Reco_Lxy_prompt",
-    "Reco_Lxyz",
-    "Reco_Lxyz_prompt",
+    ## ##"Reco_Lxy",
+    ## ##"Reco_Lxy_prompt",
+    ## ##"Reco_Lxyz",
+    ## ##"Reco_Lxyz_prompt",
     
-    "Reco_invMass",
-    "Reco_cos",
-    "Reco_DR",
+    ## ##"Reco_invMass",
+    ## ##"Reco_cos",
+    ## ##"Reco_DR",
 
-    "RecoMissingEnergy_e",
-    "RecoMissingEnergy_p",
-    "RecoMissingEnergy_pt",
-    "RecoMissingEnergy_px",
-    "RecoMissingEnergy_py",
-    "RecoMissingEnergy_pz",
-    "RecoMissingEnergy_eta",
-    "RecoMissingEnergy_theta",
-    "RecoMissingEnergy_phi",
+    ## ##"RecoMissingEnergy_e",
+    ## ##"RecoMissingEnergy_p",
+    ## ##"RecoMissingEnergy_pt",
+    ## ##"RecoMissingEnergy_px",
+    ## ##"RecoMissingEnergy_py",
+    ## ##"RecoMissingEnergy_pz",
+    ## ##"RecoMissingEnergy_eta",
+    ## ##"RecoMissingEnergy_theta",
+    ## ##"RecoMissingEnergy_phi",
 
     #"RecoPhoton_e",
     #"RecoPhoton_p",
@@ -209,50 +228,16 @@ variables = [
 #Dictionary with the analysis name as a key, and the list of selections to be plotted for this analysis. The name of the selections should be the same than in the final selection
 selections = {}
 selections['HNL']  = [
-    #"selNone",
-    #"sel2Reco_vetoes",
-
-    #"sel2RecoSF_vetoes",
-    #"sel2RecoSF_vetoes_15-80M",
-    #"sel2RecoSF_vetoes_15-80M_40p",
-    #"sel2RecoSF_vetoes_15-80M_40p_5ME",
-    #"sel2RecoSF_vetoes_10-80M_39p_5ME_cos",
-    #"sel2RecoSF_vetoes_15-80M_42p_10ME_cos_MEt",
-    "sel2RecoSF_vetoes_tracks_M80_5MEpt_p40_0.8cos",
-
-    #"sel2RecoDF_vetoes",
-    #"sel2RecoDF_vetoes_15-80M",
-    #"sel2RecoDF_vetoes_15-80M_40p",
-    #"sel2RecoDF_vetoes_15-80M_40p_5ME",
-    #"sel2RecoDF_vetoes_10-80M_39p_5ME_cos",
-    #"sel2RecoDF_vetoes_15-80M_39p_10ME43_cos",
-    #"sel2RecoDF_vetoes_tracks_M80":"sel2RecoDF_vetoes_tracks_M80",
-    "sel2RecoDF_vetoes_tracks_M80_5MEpt_0.8cos",
+    "selReco",
 ]
 
 extralabel = {}
-extralabel['selNone'] = "Before selection"
-extralabel['sel2Reco_vetoes']="Two leptons, no photons and jets"
-
-extralabel['sel2RecoSF_vetoes']="Two same flavor leptons, no photons and jets"
-extralabel['sel2RecoSF_vetoes_15-80M']="Two same flavor leptons, no photons, 15<M(l,l)<70 GeV"
-extralabel['sel2RecoSF_vetoes_15-80M_40p']="Two same flavor leptons, no photons, 15<M(l,l)<80 GeV, p<40 GeV"
-extralabel['sel2RecoSF_vetoes_15-80M_40p_5ME']="Two same flavor leptons, no photons, 15<M(l,l)<80 GeV, p<40 GeV, ME>5 Gev"
-extralabel['sel2RecoSF_vetoes_10-80M_39p_5ME_cos']="Two same flavor leptons, no photons, 10<M(l,l)<80 GeV, p<39 GeV, ME>5 Gev, cos#theta>-0.8"
-extralabel['sel2RecoSF_vetoes_15-80M_42p_10ME_cos_MEt']="Two same flavor leptons, no photons and jets, 15<M(l,l')<80 GeV, p<42 GeV, E_{miss}>10 Gev, cos#theta>-0.8, 0.2<#theta_{miss}<3"
-extralabel['sel2RecoSF_vetoes_tracks_M80_5MEpt_0.8cos']= "Two same flavor leptons, no photons and jets, two reconstructed tracks, M(l,l')<80 GeV, p_{miss}>5 GeV, cos#theta>-0.8"
-
-extralabel['sel2RecoDF_vetoes']="Two different flavor leptons, no photons and jets"
-extralabel['sel2RecoDF_vetoes_15-80M']="Two different flavor leptons, no photons, 15<M(l,l)<70 GeV"
-extralabel['sel2RecoDF_vetoes_15-80M_40p']="Two different flavor leptons, no photons, 15<M(l,l)<80 GeV, p<40 GeV"
-extralabel['sel2RecoDF_vetoes_15-80M_40p_5ME']="Two different flavor leptons, no photons, 15<M(l,l)<80 GeV, p<40 GeV, ME>5 Gev"
-extralabel['sel2RecoDF_vetoes_10-80M_39p_5ME_cos']="Two different flavor leptons, no photons, 10<M(l,l)<80 GeV, p<39 GeV, ME>5 GeV, cos#theta>-0.8"
-extralabel['sel2RecoDF_vetoes_15-80M_39p_10ME43_cos']="Two different flavor leptons, no photons and jets, 15<M(l,l')<80 GeV, p<39 GeV, 10<E_{miss}<43 GeV, cos#theta>-0.8"
-extralabel['sel2RecoDF_vetoes_tracks_M80_5MEpt_0.8cos']="Two different flavor leptons, no photons and jets, two reconstructed tracks, M(l,l')<80 GeV, p_{miss}>5 GeV, cos#theta>-0.8"
+extralabel['selReco'] = "Before selection"
 
 colors = {}
 
-colors['HNL_4e-8_10gev'] = ROOT.kCyan-7
+colors['HNL_4e-10_20gev'] = ROOT.kWhite ## ## mod edo
+#colors['HNL_4e-8_10gev'] = ROOT.kCyan-7 ## ## mod edo
 colors['HNL_1.33e-9_20gev'] = ROOT.kAzure+5
 colors['HNL_2.86e-12_30gev'] = ROOT.kBlue-7
 colors['HNL_2.86e-7_30gev'] = ROOT.kOrange-2
@@ -292,34 +277,36 @@ colors['tatanunu'] = 38
 
 plots = {}
 plots['HNL'] = {'signal':{
-                    'HNL_4e-8_10gev':['HNL_4e-8_10gev'],
+                    #'HNL_4e-8_10gev':['HNL_4e-8_10gev'], ## ## mod edo: 8_10gev -> 10_20gev
                     'HNL_1.33e-9_20gev':['HNL_1.33e-9_20gev'],
                     'HNL_2.86e-12_30gev':['HNL_2.86e-12_30gev'],
-                    'HNL_2.86e-7_30gev':['HNL_2.86e-7_30gev'],
-                    'HNL_5e-12_40gev':['HNL_5e-12_40gev'],
+                    #'HNL_2.86e-7_30gev':['HNL_2.86e-7_30gev'],
+                    #'HNL_5e-12_40gev':['HNL_5e-12_40gev'],
                     'HNL_4e-12_50gev':['HNL_4e-12_50gev'],
                     'HNL_6.67e-8_60gev':['HNL_6.67e-8_60gev'],
-                    'HNL_4e-8_60gev':['HNL_4e-8_60gev'],
+                    #'HNL_4e-8_60gev':['HNL_4e-8_60gev'],
                     'HNL_2.86e-9_70gev':['HNL_2.86e-9_70gev'],
-                    'HNL_2.86e-8_80gev':['HNL_2.86e-8_80gev'],
+                    #'HNL_2.86e-8_80gev':['HNL_2.86e-8_80gev'],
                 },
                 'backgrounds':{
+                    'HNL_4e-10_20gev':['HNL_4e-10_20gev']
                     #'HNL':['HNL_2.86e-12_30gev'], ### impossible to plot without both signals and backgrounds, choose one signal and make it white ### 
-                    'Zud': ['p8_ee_Zud_ecm91'],
-                    'Zss':['p8_ee_Zss_ecm91'],
-                    'Zcc': ['p8_ee_Zcc_ecm91'],
-                    'Zbb':['p8_ee_Zbb_ecm91'],
-                    'Zee':['p8_ee_Zee_ecm91'],
-                    'Zmumu': ['p8_ee_Zmumu_ecm91'],
-                    'Ztautau': ['p8_ee_Ztautau_ecm91'],
-                    'tatanunu': ['tatanunu'],
-                    'emununu': ['emununu'],
+                    #'Zud': ['p8_ee_Zud_ecm91'],
+                    #'Zss':['p8_ee_Zss_ecm91'],
+                    #'Zcc': ['p8_ee_Zcc_ecm91'],
+                    #'Zbb':['p8_ee_Zbb_ecm91'],
+                    #'Zee':['p8_ee_Zee_ecm91'],
+                    #'Zmumu': ['p8_ee_Zmumu_ecm91'],
+                    #'Ztautau': ['p8_ee_Ztautau_ecm91'],
+                    #'tatanunu': ['tatanunu'],
+                    #'emununu': ['emununu'],
                 },
                 }
 
 legend = {}
 
-legend['HNL_4e-8_10gev'] = 'U^{2}=4e-8, M_{N}=10 GeV'
+legend['HNL_4e-10_20gev'] = 'U^{2}=4e-10, M_{N}=20 GeV' ## mod edo
+#legend['HNL_4e-8_10gev'] = 'U^{2}=4e-8, M_{N}=10 GeV' ## ## mod edo
 legend['HNL_1.33e-9_20gev'] = 'U^{2}=1.33e-9, M_{N}=20 GeV'
 legend['HNL_2.86e-12_30gev'] = 'U^{2}=2.86e-12, M_{N}=30 GeV'
 legend['HNL_2.86e-7_30gev'] = 'U^{2}=2.86e-7, M_{N}=30 GeV'
