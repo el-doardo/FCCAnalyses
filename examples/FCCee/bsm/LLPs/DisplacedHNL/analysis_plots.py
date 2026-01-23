a997a33905e7866d86902cd7d1c8fbefd5e6968a
el-doardo <edoardospoto96.es@gmail.com>
2026-01-23 11:01:01 +0100

    Update analysis_plots.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_plots.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_plots.py
@@ -327,13 +327,13 @@ plots['HNL'] = {'signal':{
                     'HNL_4e-10_20gev':['HNL_4e-10_20gev'], ## ## mod da me
                     'HNL_1.33e-9_20gev':['HNL_1.33e-9_20gev'],
                     'HNL_2.86e-12_30gev':['HNL_2.86e-12_30gev'],
-                    'HNL_2.86e-7_30gev':['HNL_2.86e-7_30gev'],
+                    #'HNL_2.86e-7_30gev':['HNL_2.86e-7_30gev'],
                     ## ##'HNL_5e-12_40gev':['HNL_5e-12_40gev'],
                     'HNL_4e-12_50gev':['HNL_4e-12_50gev'],
                     'HNL_6.67e-8_60gev':['HNL_6.67e-8_60gev'],
-                    'HNL_4e-8_60gev':['HNL_4e-8_60gev'],
+                    #'HNL_4e-8_60gev':['HNL_4e-8_60gev'],
                     'HNL_2.86e-9_70gev':['HNL_2.86e-9_70gev'],
-                    'HNL_2.86e-8_80gev':['HNL_2.86e-8_80gev'],
+                    #'HNL_2.86e-8_80gev':['HNL_2.86e-8_80gev'],
                 },
                 'backgrounds':{
                     #'HNL':['HNL_2.86e-12_30gev'], ### impossible to plot without both signals and backgrounds, choose one signal and make it white ### 
