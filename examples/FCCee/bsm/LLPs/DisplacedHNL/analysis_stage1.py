dba5029369392b2c93a09f3aa45097066c3a5cd6
el-doardo <edoardospoto96.es@gmail.com>
2026-01-23 12:25:54 +0100

    Update analysis_stage1.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
@@ -669,7 +669,7 @@ class RDFanalysis():
                 	#.Define("FSGenMissing_e", " if (n_FSGenLepton==2) return float(91 - FSGen_TwoLeptons_energy); else return float(-1);")
 				
 					.Define("FSGenMissing_e", " float Tot=0.0; for (int i = 0; i < n_FSGenNeutrino; ++i) { Tot += FSGenNeutrino_e.at(i);} return Tot;") ## ## missing energy come energia totale dei neutrini
-					.Define("FSGenTotal_e", " float Tot=0.0; for (int i = 0; i < n_FSGenNeutrino; ++i) { Tot += FSGenNeutrino_e.at(i);}; for (int i = 0; i < n_FSGenLepton; ++i) { Tot += FSGenLepton_e.at(i);} return Tot;") ## ## spero che facci 91Gev 
+					.Define("FSGenTotal_e", " float Tot=0.0; for (int i = 0; i < n_FSGenNeutrino; ++i) { Tot += FSGenNeutrino_e.at(i);}; for (int i = 0; i < n_FSGenPhoton; ++i) { Tot += FSGenPhoton_e.at(i);}; for (int i = 0; i < n_FSGenLepton; ++i) { Tot += FSGenLepton_e.at(i);} return Tot;") ## ## spero che facci 91Gev 
 					
                 
 
