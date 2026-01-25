3bc468cc8dd9e62c7e11b116aedbdb802cf5ff02
el-doardo <edoardospoto96.es@gmail.com>
2026-01-25 19:21:26 +0100

    Update analysis_final.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_final.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_final.py
@@ -1001,6 +1001,7 @@ histoList = {
     "FSGenMissing_e":            {"name":"FSGenMissing_e",              "title":"Gen missing e [GeV]",            "bin":100,"xmin":0 ,"xmax":100},
     "FSGenMissing_pt":            {"name":"FSGenMissing_pt",              "title":"Gen missing pt [GeV]",            "bin":100,"xmin":0 ,"xmax":50},
 	"FSGenMissing_e_nu":			{"name":"FSGenMissing_e_nu",              "title":"Gen missing energy (from #nu s) [GeV]",            "bin":100,"xmin":0 ,"xmax":100},
+	"FSGenEmiss_e": 			{"name":"FSGenEmiss_e",              "title":"Gen missing energy (total - gen particles) [GeV]",            "bin":100,"xmin":0 ,"xmax":100},
 	
     #"FSGenPhoton_e":                   {"name":"FSGenPhoton_e",                  "title":"Final state gen photons energy [GeV]",       "bin":100,"xmin":0 ,"xmax":50},
     #"FSGenPhoton_p":                   {"name":"FSGenPhoton_p",                  "title":"Final state gen photons p [GeV]",            "bin":100,"xmin":0 ,"xmax":50},
