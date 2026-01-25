8f6eb734c4b55a41c19ec4b8e2d6e5e7004fdeda
el-doardo <edoardospoto96.es@gmail.com>
2026-01-25 18:38:02 +0100

    Update analysis_stage1.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
@@ -938,6 +938,9 @@ class RDFanalysis():
                 .Define("RecoEmiss_pt",  "return sqrt(RecoEmiss_px*RecoEmiss_px + RecoEmiss_py*RecoEmiss_py)")
                 .Define("RecoEmiss_p",  "return sqrt(RecoEmiss_px*RecoEmiss_px + RecoEmiss_py*RecoEmiss_py + RecoEmiss_pz*RecoEmiss_pz)")
                 .Define("RecoEmiss_e",   "RecoEmiss[0].energy")
+				
+				## ## Define for a new class of merged leptons and photons to use as input for missingEnergy function at gen lev
+				.Defien("FSGenDParticle","FCCAnalyses::MCParticle::mergeParticles(FSGenLepton, FSGenPhoton)")
 
                 ### dilepton invariant mass ###
                 .Define("Reco_TwoLeptons_energy", "if (n_RecoLeptons>1) return (Reco_e.at(0) + Reco_e.at(1)); else return float(-1.);")
