383a5536392f87bf3ac8bcf8c0eb6e57643e4bd4
el-doardo <edoardospoto96.es@gmail.com>
2026-02-21 18:04:21 +0100

    Uncomment HNL parameters and update input directory

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
@@ -23,41 +23,41 @@ processList = {
 
 	## ##
 	
-		#"HNL_1.04e-8_30gev":{},
-		#"HNL_1.04e-8_60gev":{},
-		#"HNL_1.04e-8_90gev":{},
-		#"HNL_1.04e-8_110gev":{},
-		#"HNL_1.04e-8_120gev":{},
+		"HNL_1.04e-8_30gev":{},
+		"HNL_1.04e-8_60gev":{},
+		"HNL_1.04e-8_90gev":{},
+		"HNL_1.04e-8_110gev":{},
+		"HNL_1.04e-8_120gev":{},
 	
-		#"HNL_4e-10_30gev":{},
-		#"HNL_4e-10_60gev":{},
-		#"HNL_4e-10_90gev":{},
-		#"HNL_4e-10_110gev":{},
-		#"HNL_4e-10_120gev":{},
+		"HNL_4e-10_30gev":{},
+		"HNL_4e-10_60gev":{},
+		"HNL_4e-10_90gev":{},
+		"HNL_4e-10_110gev":{},
+		"HNL_4e-10_120gev":{},
 	
-		#"HNL_6.67e-10_30gev":{},
-		#"HNL_6.67e-10_60gev":{},
-		#"HNL_6.67e-10_90gev":{},
-		#"HNL_6.67e-10_110gev":{},
-		#"HNL_6.67e-10_120gev":{},
+		"HNL_6.67e-10_30gev":{},
+		"HNL_6.67e-10_60gev":{},
+		"HNL_6.67e-10_90gev":{},
+		"HNL_6.67e-10_110gev":{},
+		"HNL_6.67e-10_120gev":{},
 
-		#"HNL_8.35e-9_30gev":{},
-		#"HNL_8.35e-9_60gev":{},
-		#"HNL_8.35e-9_90gev":{},
-		#"HNL_8.35e-9_110gev":{},
-		#"HNL_8.35e-9_120gev":{},
+		"HNL_8.35e-9_30gev":{},
+		"HNL_8.35e-9_60gev":{},
+		"HNL_8.35e-9_90gev":{},
+		"HNL_8.35e-9_110gev":{},
+		"HNL_8.35e-9_120gev":{},
 		
-		#"HNL_2.27e-9_30gev":{},
-		#"HNL_2.27e-9_60gev":{},
-		#"HNL_2.27e-9_90gev":{},
-		#"HNL_2.27e-9_110gev":{},
-		#"HNL_2.27e-9_120gev":{},
+		"HNL_2.27e-9_30gev":{},
+		"HNL_2.27e-9_60gev":{},
+		"HNL_2.27e-9_90gev":{},
+		"HNL_2.27e-9_110gev":{},
+		"HNL_2.27e-9_120gev":{},
 
-		#"HNL_3.17e-11_30gev":{},
-		#"HNL_3.17e-11_60gev":{},
-		#"HNL_3.17e-11_90gev":{},
-		#"HNL_3.17e-11_110gev":{},
-		#"HNL_3.17e-11_120gev":{},
+		"HNL_3.17e-11_30gev":{},
+		"HNL_3.17e-11_60gev":{},
+		"HNL_3.17e-11_90gev":{},
+		"HNL_3.17e-11_110gev":{},
+		"HNL_3.17e-11_120gev":{},
 }
 
 processList_ = {
@@ -469,13 +469,13 @@ processList_ = {
 #Production tag. This points to the yaml files for getting sample statistics
 #Mandatory when running over EDM4Hep centrally produced events
 #Comment out when running over privately produced events
-prodTag     = "FCCee/winter2023/IDEA/"
+#prodTag     = "FCCee/winter2023/IDEA/"
 
 #Input directory
 #Comment out when running over centrally produced events
 #Mandatory when running over privately produced events
 #inputDir = "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/"
-#inputDir = "/eos/user/e/espoto/H_pole/HNL_signals"
+inputDir = "/eos/user/e/espoto/H_pole/HNL_signals"
 
 # additional/costom C++ functions, defined in header files (optional)
 includePaths = ["functions.h"]
