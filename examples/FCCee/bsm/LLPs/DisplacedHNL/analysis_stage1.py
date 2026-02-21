8f0d0f8a86d6e5f4e1bf9840b59960151e599c0f
el-doardo <edoardospoto96.es@gmail.com>
2026-02-21 18:03:21 +0100

    Comment out 'p8_ee_Z_Zqq_ecm125' entry
    Comment out the 'p8_ee_Z_Zqq_ecm125' entry due to YAML file error.

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
@@ -19,7 +19,7 @@ processList = {
 		#'wzp6_ee_eenunu_ecm125':{'fraction':0.05}, #cross section: 0.6574
 		#'wzp6_ee_mumununu_ecm125':{'fraction':0.05}, #cross section: 0.2202
 		#'p8_ee_Z_Ztautau_ecm125':{'fraction':0.05}, #cross section: 26  # Error with this line: "Can't find the YAML file with process info for process "p8_ee_Z_Ztautau_ecm125"!"
-		'p8_ee_Z_Zqq_ecm125':{'fraction':0.05}, #cross section: 237
+		#'p8_ee_Z_Zqq_ecm125':{'fraction':0.05}, #cross section: 237 	# Error with this line: "Can't find the YAML file with process info for process "p8_ee_Z_Ztautau_ecm125"!"
 
 	## ##
 	
