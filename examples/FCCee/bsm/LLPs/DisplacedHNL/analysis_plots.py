43c7b15001e5044a37eb0ae98a533f21de3719d9
el-doardo <edoardospoto96.es@gmail.com>
2026-02-21 15:51:38 +0100

    Comment out FSGenElectron and FSGenMuon variables

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_plots.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_plots.py
@@ -29,8 +29,8 @@ variables = [
     #"FSGenMissing_e", ## ## definito da me in stage1
 	#"FSGenMissing_pt", ## ## definito da me in stage1
 	#"FSGenMissing_e_nu",
-    "n_FSGenElectron",
-    "n_FSGenMuon",
+    #"n_FSGenElectron",
+    #"n_FSGenMuon",
     "n_FSGenLepton",
     "n_GenN",
     #"n_FSGenPhoton",
