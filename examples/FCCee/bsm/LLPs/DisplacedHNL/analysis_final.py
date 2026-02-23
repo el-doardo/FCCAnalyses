8dc4ab4f167f14342917925b0bca5d03937e058f
el-doardo <edoardospoto96.es@gmail.com>
2026-02-23 13:19:30 +0100

    Update analysis_final.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_final.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_final.py
@@ -33,6 +33,26 @@ processList = {
 	#'p8_ee_Z_Ztautau_ecm125':{}, #cross section: 26  # Error with this line: "Can't find the YAML file with process info for process "p8_ee_Z_Ztautau_ecm125"!"
 	#'p8_ee_Z_Zqq_ecm125':{}, #cross section: 237 	# Error with this line: "Can't find the YAML file with process info for process "p8_ee_Z_Ztautau_ecm125"!"
 
+	## ## selezione per provarli tutti
+		'wzp6_ee_eeqq_ecm125':{},
+		'wzp6_ee_qq_ecm125':{},
+		'wzp6_ee_mumuqq_ecm125':{},
+		'wzp6_ee_tautauqq_ecm125':{},
+		'wzp6_ee_enueqq_ecm125':{},
+		'wzp6_ee_taunutauqq_ecm125':{},
+		'wzp6_ee_munumuqq_ecm125':{},
+		'p8_ee_ZZ_4tau_ecm125':{},
+		'wzp6_ee_Hbb_ecm125':{},
+		'wzp6_ee_Hgg_ecm125':{},
+		'wzp6_ee_Hlnuqq_ecm125':{},
+		'wzp6_ee_Hqqlnu_ecm125':{},
+		'wzp6_ee_Hqqenue_ecm125':{},
+		'wzp6_ee_Hqqmunumu_ecm125':{},
+		'wzp6_ee_Hqqtaunutau_ecm125':{},
+		'wzp6_ee_Hmunumuqq_ecm125':{},
+		'wzp6_ee_Henueqq_ecm125':{},
+		'wzp6_ee_Htaunutauqq_ecm125':{},
+
         
     ### privately produced backgrounds ###
     #'eenunu_m':{},
