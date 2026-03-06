904c2435a643c8c4e4d2d0f4753bca5a5d3c6e66
el-doardo <edoardospoto96.es@gmail.com>
2026-03-06 17:11:42 +0100

    Update analysis_stage1.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
@@ -953,7 +953,7 @@ class RDFanalysis():
                 #.Filter("n_RecoPhotons==0 && n_RecoLeptons==2 && ((Reco_charge.at(0)==1 && Reco_charge.at(1)==-1) || (Reco_charge.at(0)==-1 && Reco_charge.at(1)==1))") ## ## lui va via
                 ### generator selection on llnunu background that needs to be applied consinstently to the others
                 #.Filter("Reco_pt.at(0) > 1 && Reco_pt.at(1) > 1 && RecoEmiss_pt > 5")
-				.Filter("n_RecoMuons > 0 && RecoMuon_pt.at(0) > 2")
+				.Filter("n_RecoMuons > 0 && RecoMuon_pt.at(0) > 0")
 
                )
                 return df2
