d6a104601e3bdc611a9393e91846dd3e75531717
el-doardo <edoardospoto96.es@gmail.com>
2026-03-06 20:56:36 +0100

    Update analysis_final.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_final.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_final.py
@@ -1266,7 +1266,7 @@ histoList = {
 	#"PrimaryTracks":						{"name":"PrimaryTracks",    		"title":"PrimaryTracks",          		 "bin":5, "xmin":0,"xmax":5},
     #"PrimaryVertexObject":					{"name":"PrimaryVertexObject",    	"title":"PrimaryVertexObject",       	 "bin":100, "xmin":0,"xmax":90},
     "n_PrimaryTracks":						{"name":"n_PrimaryTracks",    		"title":"n_PrimaryTracks",          	 "bin":10, "xmin":0,"xmax":10},
-    #"SecondaryTracks":						{"name":"SecondaryTracks",    		"title":"SecondaryTracks",          	 "bin":10, "xmin":0,"xmax":5},
+    "SecondaryTracks":						{"name":"SecondaryTracks",    		"title":"SecondaryTracks",          	 "bin":10, "xmin":0,"xmax":5},
 	#"n_SecondaryTracks":					{"name":"n_SecondaryTracks",    	"title":"Jn_SecondaryTracks",          	 "bin":100, "xmin":0,"xmax":90},
 	
     
