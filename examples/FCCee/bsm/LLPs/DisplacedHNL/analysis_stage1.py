e48bd4b3cb1207bd9e2c65484785496e4835a9e6
el-doardo <edoardospoto96.es@gmail.com>
2026-03-08 19:01:53 +0100

    Update analysis_stage1.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
@@ -14,26 +14,26 @@ import ROOT
 #Mandatory: List of processes
 
 processList = {
-        'p8_ee_Zee_ecm91':{'fraction':0.01}, ## ## secondo il paper lui non serve, ma sono curioso
-        'p8_ee_Zmumu_ecm91':{'fraction':0.01},
-        'p8_ee_Ztautau_ecm91':{'fraction':0.01},
-        'p8_ee_Zbb_ecm91':{'fraction':0.01},
-        'p8_ee_Zcc_ecm91':{'fraction':0.01},
-        'p8_ee_Zud_ecm91':{'fraction':0.01},
-        'p8_ee_Zss_ecm91':{'fraction':0.01},
+        #'p8_ee_Zee_ecm91':{'fraction':0.01}, ## ## secondo il paper lui non serve, ma sono curioso
+        #'p8_ee_Zmumu_ecm91':{'fraction':0.01},
+        #'p8_ee_Ztautau_ecm91':{'fraction':0.01},
+        #'p8_ee_Zbb_ecm91':{'fraction':0.01},
+        #'p8_ee_Zcc_ecm91':{'fraction':0.01},
+        #'p8_ee_Zud_ecm91':{'fraction':0.01},
+        #'p8_ee_Zss_ecm91':{'fraction':0.01},
 
 	## ##
 	
-		#"HNL_1.04e-8_10gev":{},
-		#"HNL_1.04e-8_70gev":{},
-		#"HNL_4e-10_20gev":{},
-		#"HNL_4e-10_80gev":{},	
-		#"HNL_6.67e-10_30gev":{},
-		#"HNL_8.35e-9_40gev":{},
-		#"HNL_2.27e-9_20gev":{},
-		#"HNL_2.27e-9_50gev":{},
-		#"HNL_3.17e-11_30gev":{},
-		#"HNL_3.17e-11_60gev":{},
+		"HNL_1.04e-8_10gev":{},
+		"HNL_1.04e-8_70gev":{},
+		"HNL_4e-10_20gev":{},
+		"HNL_4e-10_80gev":{},	
+		"HNL_6.67e-10_30gev":{},
+		"HNL_8.35e-9_40gev":{},
+		"HNL_2.27e-9_20gev":{},
+		"HNL_2.27e-9_50gev":{},
+		"HNL_3.17e-11_30gev":{},
+		"HNL_3.17e-11_60gev":{},
 	
 	
 		#"numujj":{},
@@ -116,13 +116,13 @@ processList_ = {
 #Production tag. This points to the yaml files for getting sample statistics
 #Mandatory when running over EDM4Hep centrally produced events
 #Comment out when running over privately produced events
-prodTag     = "FCCee/winter2023/IDEA/"
+#prodTag     = "FCCee/winter2023/IDEA/"
 
 #Input directory
 #Comment out when running over centrally produced events
 #Mandatory when running over privately produced events
 
-#inputDir = "/eos/user/e/espoto/FCC_2jet_z_pole/FCCAnalysis/hadronized_signals"
+inputDir = "/eos/user/e/espoto/FCC_2jet_z_pole/FCCAnalysis/hadronized_signals"
 
 # additional/costom C++ functions, defined in header files (optional)
 includePaths = ["functions.h"]
