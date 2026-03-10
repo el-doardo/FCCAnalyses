f0947e74d47c73afefc6a727d3d965da81bc6040
el-doardo <edoardospoto96.es@gmail.com>
2026-03-10 15:55:09 +0100

    Update analysis_stage1.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
@@ -14,13 +14,13 @@ import ROOT
 #Mandatory: List of processes
 
 processList = {
-        'p8_ee_Zee_ecm91':{'fraction':1}, ## ## secondo il paper lui non serve, ma sono curioso 	## ## fraction limit sperimentato: 1
-        'p8_ee_Zmumu_ecm91':{'fraction':0.25},		## ## fraction limit sperimentato: 0.25
-        'p8_ee_Ztautau_ecm91':{'fraction':0.25},	## ## fraction limit sperimentato: 0.25
-        'p8_ee_Zbb_ecm91':{'fraction':0.1},		## ## fraction limit sperimentato: 0.1
-        'p8_ee_Zcc_ecm91':{'fraction':0.1},		## ## fraction limit sperimentato: 0.1
-        'p8_ee_Zud_ecm91':{'fraction':0.5},		## ## fraction limit sperimentato: 0.5 forse alzabile
-        'p8_ee_Zss_ecm91':{'fraction':0.5},		## ## fraction limit sperimentato: 0.5 forrse alzabile
+        #'p8_ee_Zee_ecm91':{'fraction':1}, ## ## secondo il paper lui non serve, ma sono curioso 	## ## fraction limit sperimentato: 1
+        #'p8_ee_Zmumu_ecm91':{'fraction':0.25},		## ## fraction limit sperimentato: 0.25
+        #'p8_ee_Ztautau_ecm91':{'fraction':0.25},	## ## fraction limit sperimentato: 0.25
+        #'p8_ee_Zbb_ecm91':{'fraction':0.1},		## ## fraction limit sperimentato: 0.1
+        #'p8_ee_Zcc_ecm91':{'fraction':0.1},		## ## fraction limit sperimentato: 0.1
+        #'p8_ee_Zud_ecm91':{'fraction':0.5},		## ## fraction limit sperimentato: 0.5 forse alzabile
+        #'p8_ee_Zss_ecm91':{'fraction':0.5},		## ## fraction limit sperimentato: 0.5 forrse alzabile
 
 	## ##
 	
@@ -28,8 +28,8 @@ processList = {
 		#"HNL_1.04e-8_70gev":{},
 		#"HNL_4e-10_20gev":{},
 		#"HNL_4e-10_80gev":{},	
-		#"HNL_6.67e-10_30gev":{},
-		#"HNL_8.35e-9_40gev":{},
+		"HNL_6.67e-10_30gev":{},
+		"HNL_8.35e-9_40gev":{},
 		#"HNL_2.27e-9_20gev":{},
 		#"HNL_2.27e-9_50gev":{},
 		#"HNL_3.17e-11_30gev":{},
@@ -37,7 +37,7 @@ processList = {
 
 	
 	
-		#"numujj":{},
+		"numujj":{},
 }
 
 processList_ = {
@@ -522,7 +522,8 @@ class RDFanalysis():
 				.Define("RecoMuon_lead_Track_absD0", "return abs(ReconstructedParticle2Track::getRP2TRK_D0(RecoMuon_lead,EFlowTrack_1))")
 				.Define("RecoMuon_lead_Track", "ReconstructedParticle2Track::getRP2TRK(RecoMuon_lead,EFlowTrack_1)") ## ## inserisco questa traccia in get_VertexData per ottenere un oggetto sul quale poter fare il taglio al chi2
 				.Define("RecoDecayVertexObjectMuon_lead",   "VertexFitterSimple::VertexFitter_Tk( 0, RecoMuon_lead_Track)" ) ### reconstructing a vertex withour any request n=0 ###
-                .Define("RecoDecayVertexMuon_lead",  "VertexingUtils::get_VertexData( RecoDecayVertexObjectMuon_lead )") ## ## se tutto va bene userò il chi2 da questo oggetto
+                #.Define("RecoDecayVertexMuon_lead",  "VertexingUtils::get_VertexData( RecoDecayVertexObjectMuon_lead )") ## ## se tutto va bene userò il chi2 da questo oggetto
+				.Define("RecoDecayVertexMuon_lead",  "VertexingUtils::get_VertexData( RecoDecayVertexObjectMuon_lead, EFlowTrack_1 )") ## ## provo a usare questo
 			
                 ### Jet clustering with different algorithm, only on non leptons ###
                 ### https://github.com/HEP-FCC/FCCAnalyses/blob/master/addons/FastJet/JetClustering.h ###
