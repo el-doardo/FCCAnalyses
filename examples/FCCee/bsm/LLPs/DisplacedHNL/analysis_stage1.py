ba0124d5ef3832bb22e8ec1d7f34a23949da6026
el-doardo <edoardospoto96.es@gmail.com>
2026-03-10 16:43:17 +0100

    Update analysis_stage1.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
@@ -14,13 +14,13 @@ import ROOT
 #Mandatory: List of processes
 
 processList = {
-        #'p8_ee_Zee_ecm91':{'fraction':1}, ## ## secondo il paper lui non serve, ma sono curioso 	## ## fraction limit sperimentato: 1
-        #'p8_ee_Zmumu_ecm91':{'fraction':0.25},		## ## fraction limit sperimentato: 0.25
-        #'p8_ee_Ztautau_ecm91':{'fraction':0.25},	## ## fraction limit sperimentato: 0.25
-        #'p8_ee_Zbb_ecm91':{'fraction':0.1},		## ## fraction limit sperimentato: 0.1
-        #'p8_ee_Zcc_ecm91':{'fraction':0.1},		## ## fraction limit sperimentato: 0.1
-        #'p8_ee_Zud_ecm91':{'fraction':0.5},		## ## fraction limit sperimentato: 0.5 forse alzabile
-        #'p8_ee_Zss_ecm91':{'fraction':0.5},		## ## fraction limit sperimentato: 0.5 forrse alzabile
+        'p8_ee_Zee_ecm91':{'fraction':0.01}, ## ## secondo il paper lui non serve, ma sono curioso 	## ## fraction limit sperimentato: 1
+        'p8_ee_Zmumu_ecm91':{'fraction':0.01},		## ## fraction limit sperimentato: 0.25
+        'p8_ee_Ztautau_ecm91':{'fraction':0.01},	## ## fraction limit sperimentato: 0.25
+        'p8_ee_Zbb_ecm91':{'fraction':0.01},		## ## fraction limit sperimentato: 0.1
+        'p8_ee_Zcc_ecm91':{'fraction':0.01},		## ## fraction limit sperimentato: 0.1
+        'p8_ee_Zud_ecm91':{'fraction':0.01},		## ## fraction limit sperimentato: 0.5 forse alzabile
+        'p8_ee_Zss_ecm91':{'fraction':0.01},		## ## fraction limit sperimentato: 0.5 forrse alzabile
 
 	## ##
 	
@@ -28,8 +28,8 @@ processList = {
 		#"HNL_1.04e-8_70gev":{},
 		#"HNL_4e-10_20gev":{},
 		#"HNL_4e-10_80gev":{},	
-		"HNL_6.67e-10_30gev":{},
-		"HNL_8.35e-9_40gev":{},
+		#"HNL_6.67e-10_30gev":{},
+		#"HNL_8.35e-9_40gev":{},
 		#"HNL_2.27e-9_20gev":{},
 		#"HNL_2.27e-9_50gev":{},
 		#"HNL_3.17e-11_30gev":{},
@@ -37,7 +37,7 @@ processList = {
 
 	
 	
-		"numujj":{},
+		#"numujj":{},
 }
 
 processList_ = {
@@ -123,7 +123,7 @@ processList_ = {
 #Comment out when running over centrally produced events
 #Mandatory when running over privately produced events
 
-inputDir = "/eos/user/e/espoto/FCC_2jet_z_pole/FCCAnalysis/hadronized_signals"
+#inputDir = "/eos/user/e/espoto/FCC_2jet_z_pole/FCCAnalysis/hadronized_signals"
 
 # additional/costom C++ functions, defined in header files (optional)
 includePaths = ["functions.h"]
