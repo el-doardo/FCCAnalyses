c0a1fccea36fbaeae8a84583f7637333576ecd03
el-doardo <edoardospoto96.es@gmail.com>
2026-03-11 19:49:02 +0100

    Update analysis_plots.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_plots.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_plots.py
@@ -237,8 +237,7 @@ variables = [
 	"RecoMuon_lead_Track_absD0",
 	"RecoMuon_lead_Track_absD0_mid",
 	"RecoMuon_lead_Track_absD0_prompt",
-	"RecoDecayVertexMuon_lead.chi2",
-	"RecoDecayVertexMuon_lead.chi2_10e6",
+	"RecoDecayVertexMuon_lead_chi2",
 	"n_RecoTracks",
 	"n_PrimaryTracks",
 	"n_SecondaryTracks",
@@ -251,6 +250,7 @@ variables = [
 	"RecoMissingEnergy_eta",
 	"RecoMissingEnergy_theta",
 	"RecoMissingEnergy_phi",
+	"Reco_DecayVertexMuon_lead_z"
 ]
 
     
