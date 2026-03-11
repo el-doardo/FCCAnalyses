dcd263dce1b9286b9910630190ec71c2655397ea
el-doardo <edoardospoto96.es@gmail.com>
2026-03-11 19:55:44 +0100

    Update analysis_final.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_final.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_final.py
@@ -939,6 +939,6 @@ histoList = {
 	"RecoMuon_lead_Track_absD0_prompt":		{"name":"RecoMuon_lead_Track_absD0",    	"title":"RecoMuon_lead_Track_absD0",          	 "bin":50, "xmin":0,"xmax":1},
 	#"RecoDecayVertexMuon_lead_chi2":		{"name":"RecoDecayVertexMuon_lead.chi2",    	"title":"RecoDecayVertexMuon_lead.chi2",          	 "bin":50, "xmin":0,"xmax":50},
 	"Reco_DecayVertexMuon_lead_z":    		{"name":"RecoDecayVertexMuon_lead.position.z",  "title":"Reco decay lepton vertex z [mm]",            "bin":100,"xmin":-1 ,"xmax":2000},
-    "Reco_DecayVertexMuon_lead_chi2":       {"name":"RecoDecayVertexMuon_lead.chi2",        "title":"Reco decay lepton vertex #chi^{2}",          "bin":100,"xmin":0 ,"xmax":20},
+    "Reco_DecayVertexMuon_lead.chi2":       {"name":"RecoDecayVertexMuon_lead.chi2",        "title":"Reco decay lepton vertex #chi^{2}",          "bin":100,"xmin":0 ,"xmax":20},
     
 }
