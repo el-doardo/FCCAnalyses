e157b7d259a6199218263c7cf28df7454e36d18f
el-doardo <edoardospoto96.es@gmail.com>
2026-03-11 16:41:17 +0100

    Update analysis_plots.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_plots.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_plots.py
@@ -260,8 +260,8 @@ selections['HNL']  = [
     #"selReco_gen_notracks_2eh_10MET_0.8cos_80ME_10chi_0.64d0",
     #"selReco_gen_notracks_2eh_15to70ME",
     "selReco",
-	"selReco_chi2<10",
-	"selReco_chi2<5",
+	"selReco_chi2_10",
+	"selReco_chi2_5",
 
     #"sel2RecoDF_vetoes",
     #"sel2RecoDF_vetoes_15-80M",
@@ -276,8 +276,8 @@ selections['HNL']  = [
 extralabel = {}
 extralabel['selNone'] = "Before selection"
 extralabel['selReco']="Two leptons, different charge, no photons and jets"
-extralabel['selReco_chi2<10']="selReco_chi2<10"
-extralabel['selReco_chi2<5']="selReco_chi2<5"
+extralabel['selReco_chi2_10']="selReco_chi2<10"
+extralabel['selReco_chi2_5']="selReco_chi2<5"
 extralabel['sel2Reco_vetoes']="Two leptons, no photons and jets"
 extralabel['selReco_gen_notracks_2eh_10MET_0.8cos_80ME_10chi_0.64d0']="Two leptons, no photons an jets, M(l,l')<80 GeV, p_{t,miss}>10 GeV, cos#theta>-0.8" ## ## aggiunto per prova
 extralabel['selReco_gen_notracks_2eh_15to70ME']="Two leptons, no photons, no tracks, no neutral hadrons 15Gev<M(l,l')<70Gev"
