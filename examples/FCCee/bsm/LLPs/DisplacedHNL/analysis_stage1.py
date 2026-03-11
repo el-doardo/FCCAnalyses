37f1e46f7f62f54576c8d226813de001428b40b3
el-doardo <edoardospoto96.es@gmail.com>
2026-03-11 12:21:24 +0100

    Update analysis_stage1.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
@@ -24,20 +24,20 @@ processList = {
 
 	## ##
 	
-		"HNL_1.04e-8_10gev":{},
-		"HNL_1.04e-8_70gev":{},
-		"HNL_4e-10_20gev":{},
-		"HNL_4e-10_80gev":{},	
-		#"HNL_6.67e-10_30gev":{},
-		#"HNL_8.35e-9_40gev":{},
-		"HNL_2.27e-9_20gev":{},
-		"HNL_2.27e-9_50gev":{},
-		"HNL_3.17e-11_30gev":{},
-		"HNL_3.17e-11_60gev":{},
+		#"HNL_1.04e-8_10gev":{},
+		#"HNL_1.04e-8_70gev":{},
+		#"HNL_4e-10_20gev":{},
+		#"HNL_4e-10_80gev":{},	
+		"HNL_6.67e-10_30gev":{},
+		"HNL_8.35e-9_40gev":{},
+		#"HNL_2.27e-9_20gev":{},
+		#"HNL_2.27e-9_50gev":{},
+		#"HNL_3.17e-11_30gev":{},
+		#"HNL_3.17e-11_60gev":{},
 
 	
 	
-		#"numujj":{},
+		"numujj":{},
 }
 
 processList_ = {
@@ -514,44 +514,116 @@ class RDFanalysis():
 
                 ### angular distance between two leptons ###
                 .Define("Reco_DR","if (n_RecoLeptons>1) return myUtils::deltaR(Reco_phi.at(0), Reco_phi.at(1), Reco_eta.at(0), Reco_eta.at(1)); else return float(-1.);")
+			
 
-				## ## estraggo il muone a maggior pt
+			#################################################################################################################################################
+			#################################################################################################################################################
+			#################################################################################################################################################
+			## ## Da qui inserisco il grosso blocco riadattato da analysis_SV.py
+			## ## estraggo il muone a maggior pt
+			
 				.Define("RecoMuon_lead", "FCCAnalyses::ZHfunctions::get_leading_pt(RecoMuons);") ## ## definisco get_leading_pt basandomi su get_leading
 				.Define("RecoMuon_lead_pt", "ReconstructedParticle::get_pt(RecoMuon_lead) ") ## ## Pt del muone lead
 				.Define("RecoMuon_lead_e", "ReconstructedParticle::get_e(RecoMuon_lead) ") ## ## Pt del muone lead
 				.Define("RecoMuon_lead_Track_absD0", "return abs(ReconstructedParticle2Track::getRP2TRK_D0(RecoMuon_lead,EFlowTrack_1))")
-				.Define("RecoMuon_lead_Track", "ReconstructedParticle2Track::getRP2TRK(RecoMuon_lead,EFlowTrack_1)") ## ## inserisco questa traccia in get_VertexData per ottenere un oggetto sul quale poter fare il taglio al chi2
-				.Define("RecoDecayVertexObjectMuon_lead",   "VertexFitterSimple::VertexFitter_Tk( 0, RecoMuon_lead_Track)" ) ### reconstructing a vertex withour any request n=0 ###
+				#.Define("RecoMuon_lead_Track", "ReconstructedParticle2Track::getRP2TRK(RecoMuon_lead,EFlowTrack_1)") ## ## inserisco questa traccia in get_VertexData per ottenere un oggetto sul quale poter fare il taglio al chi2
+				#.Define("RecoDecayVertexObjectMuon_lead",   "VertexFitterSimple::VertexFitter_Tk( 0, RecoMuon_lead_Track)" ) ### reconstructing a vertex withour any request n=0 ###
 				#.Define("RecoDecayVertexObjectMuon_lead",   "VertexFitterSimple::VertexFitter_Tk( 0, RecoMuon_lead_Track, EFlowTrack_1 )" ) ## ## provo a usare questo invece di quello sopra (non è cambiato nulla)
                 # .Define("RecoDecayVertexMuon_lead",  "VertexingUtils::get_VertexData( RecoDecayVertexObjectMuon_lead )") ## ## se tutto va bene userò il chi2 da questo oggetto, ne ho definito uno omonimo più avanti.
+
+							# number of tracks
+               .Define("ntracks","ReconstructedParticle2Track::getTK_n(EFlowTrack_1)")
+
+
+               #####
+               # determime the primary (and secondary) tracks without using the MC-matching:
+
+	       # Select the tracks that are reconstructed  as primaries
+               .Define("RecoedPrimaryTracks",  "VertexFitterSimple::get_PrimaryTracks( EFlowTrack_1, true, 4.5, 20e-3, 300, 0., 0., 0.)")
+
+               .Define("n_RecoedPrimaryTracks",  "ReconstructedParticle2Track::getTK_n( RecoedPrimaryTracks )")
+               # the final primary vertex :
+               .Define("PrimaryVertexObject",   "VertexFitterSimple::VertexFitter_Tk ( 1, RecoedPrimaryTracks, true, 4.5, 20e-3, 300) ")
+               .Define("PrimaryVertex",   "VertexingUtils::get_VertexData( PrimaryVertexObject )")
+
+               # the secondary tracks
+               .Define("SecondaryTracks",   "VertexFitterSimple::get_NonPrimaryTracks( EFlowTrack_1,  RecoedPrimaryTracks )")
+               .Define("n_SecondaryTracks",  "ReconstructedParticle2Track::getTK_n( SecondaryTracks )" )
+
+               # which of the tracks are primary according to the reco algprithm (boolean)
+               .Define("IsPrimary_based_on_reco",  "VertexFitterSimple::IsPrimary_forTracks( EFlowTrack_1,  RecoedPrimaryTracks )")
+
+               # jet clustering (ee-kt) before reconstructing SVs in event
+				.Define("JetsParticles", "ReconstructedParticle::remove(ReconstructedParticles, RecoMuon_lead)")
+               .Define("RP_px",  "ReconstructedParticle::get_px(JetsParticles)") ## ## Sto usando JetsParticles invece di ReconstructedParticles
+               .Define("RP_py",  "ReconstructedParticle::get_py(JetsParticles)")
+               .Define("RP_pz",  "ReconstructedParticle::get_pz(JetsParticles)")
+               .Define("RP_e",   "ReconstructedParticle::get_e(JetsParticles)")
+               #build psedo-jets with the Reconstructed final particles
+               .Define("pseudo_jets", "JetClusteringUtils::set_pseudoJets(RP_px, RP_py, RP_pz, RP_e)")
+               #run jet clustering with all reco particles. ee_kt_algorithm, exclusive clustering, exactly 2 jets, E-scheme
+               .Define("FCCAnalysesJets_ee_kt", "JetClustering::clustering_ee_kt(2, 2, 1, 0)(pseudo_jets)")
+               #get the jets out of the structure
+               .Define("jets_ee_kt", "JetClusteringUtils::get_pseudoJets(FCCAnalysesJets_ee_kt)")
+               #get the jet constituents out of the structure
+	       		.Define("jetconstituents_ee_kt", "JetClusteringUtils::get_constituents(FCCAnalysesJets_ee_kt)")
+
+
+	       # finding SVs in jets 
+               .Define("SV_jet", "VertexFinderLCFIPlus::get_SV_jets(ReconstructedParticles, EFlowTrack_1, PrimaryVertexObject, IsPrimary_based_on_reco, jets_ee_kt, jetconstituents_ee_kt)") ## posso provare a inserire JetsParticles al posto di ReconstructedParticles 
+               # finding SVs in the event (two interfaces)
+	       #.Define("SV_evt1", "VertexFinderLCFIPlus::get_SV_event(ReconstructedParticles, EFlowTrack_1, PrimaryVertexObject, IsPrimary_based_on_reco)")
+               #.Define("SV_evt2", "VertexFinderLCFIPlus::get_SV_event(SecondaryTracks, EFlowTrack_1, PrimaryVertexObject)")
+
+	       # multiplicity
+               #.Define("SV_evt2_n","VertexingUtils::get_n_SV(SV_evt2)")
+               #.Define("SV_evt1_n","VertexingUtils::get_n_SV(SV_evt1)")
+	       		.Define("SV_jet_n", "VertexingUtils::get_n_SV(SV_jet)")
+               # vertex position
+               .Define("SV_jet_position",  "VertexingUtils::get_position_SV( SV_jet )")
+               #.Define("SV_evt1_position", "VertexingUtils::get_position_SV( SV_evt1 )")
+               #.Define("SV_evt2_position", "VertexingUtils::get_position_SV( SV_evt2 )")
+
+               # more SV properties
+               .Define("sv_mass",    "VertexingUtils::get_invM(SV_jet)") # SV mass
+               # .Define("sv_p",       "VertexingUtils::get_pMag_SV(SV_jet)") # SV momentum (magnitude)
+               # .Define("sv_ntracks", "VertexingUtils::get_VertexNtrk(SV_jet)") # SV daughters (no of tracks)
+               .Define("sv_chi2",    "VertexingUtils::get_chi2_SV(SV_jet)") # SV chi2 (not normalised)
+               .Define("sv_normchi2","VertexingUtils::get_norm_chi2_SV(SV_jet)") # SV chi2 (normalised)
+               # .Define("sv_ndf",     "VertexingUtils::get_nDOF_SV(SV_jet)") # SV no of DOF
+               # .Define("sv_theta",   "VertexingUtils::get_theta_SV(SV_jet)") # SV polar angle (theta)
+               # .Define("sv_phi",     "VertexingUtils::get_phi_SV(SV_jet)") # SV azimuthal angle (phi)
+
+#################################################################################################################################################
+#################################################################################################################################################
+#################################################################################################################################################
 			
                 ### Jet clustering with different algorithm, only on non leptons ###
                 ### https://github.com/HEP-FCC/FCCAnalyses/blob/master/addons/FastJet/JetClustering.h ###
                 #.Define("JetsParticles", "ReconstructedParticle::remove(ReconstructedParticles, RecoLeptons)") ## ## modifico questa riga
-				.Define("JetsParticles", "ReconstructedParticle::remove(ReconstructedParticles, RecoMuon_lead)")
-                .Define("RP_px", "ReconstructedParticle::get_px(JetsParticles) ")
-                .Define("RP_py", "ReconstructedParticle::get_py(JetsParticles) ")
-                .Define("RP_pz", "ReconstructedParticle::get_pz(JetsParticles) ")
-                .Define("RP_e", "ReconstructedParticle::get_e(JetsParticles) ")
-                # build pseudo jets with the RP, using the interface that takes px,py,pz,E
-                .Define("pseudo_jets",  "JetClusteringUtils::set_pseudoJets(RP_px, RP_py, RP_pz, RP_e)" )
-                ### Durham algo, exclusive clustering (first number 2) N_jets=0 (second number), E-scheme=0 (third and forth numbers) ###
-                .Define( "FCCAnalysesJets_ee_kt",  "JetClustering::clustering_ee_kt(2, 2, 1, 0)(pseudo_jets)" ) ## ## exclusive
-				#.Define( "FCCAnalysesJets_ee_kt",  "JetClustering::clustering_ee_kt(0, 5, 1, 0)(pseudo_jets)" ) ## ## inclusive
-                .Define("jets_ee_kt",  "JetClusteringUtils::get_pseudoJets( FCCAnalysesJets_ee_kt )")
-                ### get the number of jets in a workaround way, anyway is exactly zero for exclusive clustering ###
-                .Define("jets_e",  "JetClusteringUtils::get_e(jets_ee_kt)")
-				.Define("jets_pt",  "JetClusteringUtils::get_pt(jets_ee_kt)")
-				.Define("jets_p",  "JetClusteringUtils::get_p(jets_ee_kt)")
-				.Define("jets_px",  "JetClusteringUtils::get_px(jets_ee_kt)")
-				.Define("jets_py",  "JetClusteringUtils::get_py(jets_ee_kt)")
-				.Define("jets_pz",  "JetClusteringUtils::get_pz(jets_ee_kt)")
-				.Define("jets_m",  "JetClusteringUtils::get_m(jets_ee_kt)")
-				.Define("jets_eta",  "JetClusteringUtils::get_eta(jets_ee_kt)")
-				.Define("jets_ph",  "JetClusteringUtils::get_phi(jets_ee_kt)")
-				.Define("jets_phi_std",  "JetClusteringUtils::get_phi_std(jets_ee_kt)")
-				.Define("jets_theta",  "JetClusteringUtils::get_theta(jets_ee_kt)")
-                .Define("n_jets", "jets_e.size()")
+				## ##.Define("JetsParticles", "ReconstructedParticle::remove(ReconstructedParticles, RecoMuon_lead)")
+                ## ##.Define("RP_px", "ReconstructedParticle::get_px(JetsParticles) ")
+                ## ##.Define("RP_py", "ReconstructedParticle::get_py(JetsParticles) ")
+                ## ##.Define("RP_pz", "ReconstructedParticle::get_pz(JetsParticles) ")
+                ## ### build pseudo jets with the RP, using the interface that takes px,py,pz,E
+                ## ##.Define("pseudo_jets",  "JetClusteringUtils::set_pseudoJets(RP_px, RP_py, RP_pz, RP_e)" )
+                ## ##### Durham algo, exclusive clustering (first number 2) N_jets=0 (second number), E-scheme=0 (third and forth numbers) ###
+                ## ##.Define( "FCCAnalysesJets_ee_kt",  "JetClustering::clustering_ee_kt(2, 2, 1, 0)(pseudo_jets)" ) ## ## exclusive
+				## ###.Define( "FCCAnalysesJets_ee_kt",  "JetClustering::clustering_ee_kt(0, 5, 1, 0)(pseudo_jets)" ) ## ## inclusive
+                ## ##.Define("jets_ee_kt",  "JetClusteringUtils::get_pseudoJets( FCCAnalysesJets_ee_kt )")
+                ## ##### get the number of jets in a workaround way, anyway is exactly zero for exclusive clustering ###
+                ## ##.Define("jets_e",  "JetClusteringUtils::get_e(jets_ee_kt)")
+				## ##.Define("jets_pt",  "JetClusteringUtils::get_pt(jets_ee_kt)")
+				## ##.Define("jets_p",  "JetClusteringUtils::get_p(jets_ee_kt)")
+				## ##.Define("jets_px",  "JetClusteringUtils::get_px(jets_ee_kt)")
+				## ##.Define("jets_py",  "JetClusteringUtils::get_py(jets_ee_kt)")
+				## ##.Define("jets_pz",  "JetClusteringUtils::get_pz(jets_ee_kt)")
+				## ##.Define("jets_m",  "JetClusteringUtils::get_m(jets_ee_kt)")
+				## ##.Define("jets_eta",  "JetClusteringUtils::get_eta(jets_ee_kt)")
+				## ##.Define("jets_ph",  "JetClusteringUtils::get_phi(jets_ee_kt)")
+				## ##.Define("jets_phi_std",  "JetClusteringUtils::get_phi_std(jets_ee_kt)")
+				## ##.Define("jets_theta",  "JetClusteringUtils::get_theta(jets_ee_kt)")
+                ## ##.Define("n_jets", "jets_e.size()")
 
                 ### Durham algo, exclusive clustering (first number 2, 3 for exclusive up to n) N_jets=0 (second number), E-scheme=0 (third and forth numbers) ###
                 .Define( "FCCAnalysesJets_ee_kt_excl",  "JetClustering::clustering_ee_kt(3, 2, 1, 0)(pseudo_jets)" )
@@ -979,6 +1051,38 @@ class RDFanalysis():
 					"RecoMuon_lead_e",
 					"RecoMuon_lead_Track_absD0",
 					"RecoDecayVertexMuon_lead",
+					
+					########################################
+					########   JET   #######################
+					########################################
+					  #  primary vertex and primary tracks w/o any MC-matching :
+                		'IsPrimary_based_on_reco',
+                		'PrimaryVertex',
+
+                		# vector of SV vertex objects
+                		'SV_jet',
+                		#'SV_evt1',
+                		#'SV_evt2',
+
+                		# SV multiplicity
+               			 #'SV_evt1_n',
+                		#'SV_evt2_n',
+                		'SV_jet_n',
+
+                		# SV position
+                		'SV_jet_position',
+                		#'SV_evt1_position',
+						#'SV_evt2_position',
+
+                		# more SV variables
+                		'sv_mass',
+                		# 'sv_p',
+                		# 'sv_ntracks',
+                		'sv_chi2',
+                		'sv_normchi2',
+                		# 'sv_ndf',
+	        			# 'sv_theta',
+                		# 'sv_phi',
 
 		]
 
