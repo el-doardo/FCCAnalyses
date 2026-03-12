0c67936b6229dbc666b8e132c67e58bb40715d73
el-doardo <edoardospoto96.es@gmail.com>
2026-03-12 19:51:06 +0100

    Update analysis_plots.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_plots.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_plots.py
@@ -1,7 +1,7 @@
 import ROOT
 
 # global parameters
-intLumi        = 150.0e+06 #in pb-1
+intLumi        = 240.0e+06 #in pb-1
 
 ###If scaleSig=0 or scaleBack=0, we don't apply any additional scaling, on top of the normalization to cross section and integrated luminosity, as defined in finalSel.py
 ###If scaleSig or scaleBack is not defined, plots will be normalized to 1
