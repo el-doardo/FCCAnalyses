237f0128dbab2bec2414b5129188c91ec3c4070c
el-doardo <edoardospoto96.es@gmail.com>
2026-03-12 19:14:12 +0100

    Update analysis_final.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_final.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_final.py
@@ -504,7 +504,7 @@ cutList = {
 	#"selReco_c_mu_p_miss":"c_mu_p_miss<0.80",
 	"selReco_Ej_5":"jets_e.at(0)>5 && jets_e.at(1)>5",
 	"selReco_c_jj":"c_jj>-0.80",
-	"selreco_c_jmu_against_Z":"c_jmu<0.8 && c_muj<0.8",
+	#"selreco_c_jmu_against_Z":"c_jmu<0.8 && c_muj<0.8",
 	#"selreco_c_jmu_against_tautau":"c_jmu>-0.98 && c_muj>-0.98",
     ### cuts for SM llnunu at generator level to be applied to everything properly
     #"selReco_gen": "Reco_pt.at(0)>1 && Reco_pt.at(1)>1 && RecoEmiss_pt>5", ## Reco_e>2 is taken care of by delphes already, no need to specify it here
