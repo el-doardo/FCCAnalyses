5667caef539a2bb92d19325f33b9e62a50bc325b
el-doardo <edoardospoto96.es@gmail.com>
2026-03-12 19:39:19 +0100

    Update analysis_final.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_final.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_final.py
@@ -26,10 +26,10 @@ processList = {
     'p8_ee_Zee_ecm91':{},
     'p8_ee_Zmumu_ecm91':{},
     'p8_ee_Ztautau_ecm91':{},
-    #'p8_ee_Zbb_ecm91':{},
-    #'p8_ee_Zcc_ecm91':{},
-    #'p8_ee_Zud_ecm91':{},
-    #'p8_ee_Zss_ecm91':{},
+    'p8_ee_Zbb_ecm91':{},
+    'p8_ee_Zcc_ecm91':{},
+    'p8_ee_Zud_ecm91':{},
+    'p8_ee_Zss_ecm91':{},
         
     ### privately produced backgrounds ###
     #'eenunu_m':{},
@@ -41,9 +41,9 @@ processList = {
 
 		#"HNL_1.04e-8_10gev":{},
 		#"HNL_1.04e-8_70gev":{},
-		#"HNL_4e-10_20gev":{},
+		"HNL_4e-10_20gev":{},
 		#"HNL_4e-10_80gev":{},	
-		#"HNL_6.67e-10_30gev":{},
+		"HNL_6.67e-10_30gev":{},
 		#"HNL_8.35e-9_40gev":{},
 		#"HNL_2.27e-9_20gev":{},
 		#"HNL_2.27e-9_50gev":{},
@@ -51,7 +51,7 @@ processList = {
 		#"HNL_3.17e-11_60gev":{},
 
 	
-	#"numujj":{},
+	"numujj":{},
 }
 
 processList_ = {
