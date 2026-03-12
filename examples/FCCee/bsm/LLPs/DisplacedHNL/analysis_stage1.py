d7416f71521d30e7933e0bea0649cad0dba996ac
el-doardo <edoardospoto96.es@gmail.com>
2026-03-12 19:19:15 +0100

    Update analysis_stage1.py
    correzione a parentesi d achiudere a 320

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
@@ -317,7 +317,7 @@ class RDFanalysis():
 				.Define("c_p_miss_abs", "abs(cos(Sum(RecoMissingEnergy_theta)))")
 				.Define("c_mu_p_miss", "cos(RecoMuon_lead_theta - Sum(RecoMissingEnergy_theta))")
 				.Define("c_jj", "cos(jets_theta.at(0) - jets_theta.at(1))")
-				.Define("c_jmu", "cos(jets_theta.at(0) - RecoMuon_lead_theta.at(0)")
+				.Define("c_jmu", "cos(jets_theta.at(0) - RecoMuon_lead_theta.at(0))")
 				.Define("c_muj", "cos(RecoMuon_lead_theta.at(0) -  jets_theta.at(1))")
 
                 
