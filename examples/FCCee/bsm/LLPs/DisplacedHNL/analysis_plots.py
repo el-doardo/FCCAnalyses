7ff7865d055f1bc6a944cfcaf60ec3c178b0ec44
el-doardo <edoardospoto96.es@gmail.com>
2026-03-12 10:51:07 +0100

    Update analysis_plots.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_plots.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_plots.py
@@ -252,6 +252,36 @@ variables = [
 	"RecoMissingEnergy_phi",
 	"Reco_DecayVertexMuon_lead_z",
 	"Log_chi2",
+	
+	"Reco_DecayVertexMuon_lead_z_0P",
+	"Reco_DecayVertexMuon_lead_0P.chi2",
+	"Log_chi2_0P",
+	"Reco_Lxyz_0P",
+	"Reco_Lxyz_prompt_0P",
+
+	"Reco_DecayVertexMuon_lead_z_2P",
+	"Reco_DecayVertexMuon_lead_2P.chi2",
+	"Log_chi2_2P",
+	"Reco_Lxyz_2P",
+	"Reco_Lxyz_prompt_2P",
+
+	"Reco_DecayVertexMuon_lead_z_0S",
+	"Reco_DecayVertexMuon_lead_0S.chi2",
+	"Log_chi2_0S",
+	"Reco_Lxyz_0S",
+	"Reco_Lxyz_prompt_0S",
+
+	"Reco_DecayVertexMuon_lead_z_1S",
+	"Reco_DecayVertexMuon_lead_1S.chi2",
+	"Log_chi2_1S",
+	"Reco_Lxyz_1S",
+	"Reco_Lxyz_prompt_1S",
+
+	"Reco_DecayVertexMuon_lead_z_2S",
+	"Reco_DecayVertexMuon_lead_2S.chi2",
+	"Log_chi2_2S",
+	"Reco_Lxyz_2S",
+	"Reco_Lxyz_prompt_2S",
 ]
 
     
@@ -350,29 +380,29 @@ colors['tatanunu_m'] = 38
 plots = {}
 plots['HNL'] = {'signal':{
 	
-                    'HNL_1.04e-8_10gev':['HNL_1.04e-8_10gev'],
+                    #'HNL_1.04e-8_10gev':['HNL_1.04e-8_10gev'],
 					'HNL_4e-10_20gev':['HNL_4e-10_20gev'],
-					'HNL_4e-10_80gev':['HNL_4e-10_80gev'],
-					'HNL_6.67e-10_30gev':['HNL_6.67e-10_30gev'],
-					'HNL_8.35e-9_40gev':['HNL_8.35e-9_40gev'],
-					'HNL_2.27e-9_20gev':['HNL_2.27e-9_20gev'],
-                    'HNL_1.04e-8_70gev':['HNL_1.04e-8_70gev'],
-					'HNL_2.27e-9_50gev':['HNL_2.27e-9_50gev'],
-					'HNL_3.17e-11_30gev':['HNL_3.17e-11_30gev'],
-					'HNL_3.17e-11_60gev':['HNL_3.17e-11_60gev'],
+					#'HNL_4e-10_80gev':['HNL_4e-10_80gev'],
+					#'HNL_6.67e-10_30gev':['HNL_6.67e-10_30gev'],
+					#'HNL_8.35e-9_40gev':['HNL_8.35e-9_40gev'],
+					#'HNL_2.27e-9_20gev':['HNL_2.27e-9_20gev'],
+                    #'HNL_1.04e-8_70gev':['HNL_1.04e-8_70gev'],
+					#'HNL_2.27e-9_50gev':['HNL_2.27e-9_50gev'],
+					#'HNL_3.17e-11_30gev':['HNL_3.17e-11_30gev'],
+					#'HNL_3.17e-11_60gev':['HNL_3.17e-11_60gev'],
 
                 },
                 'backgrounds':{
                     #'HNL':['HNL_2.86e-12_30gev'], ### impossible to plot without both signals and backgrounds, choose one signal and make it white ### 
 
 					'numujj':['numujj'],
-					'Zee':['p8_ee_Zee_ecm91'],
-   					'Zmumu':['p8_ee_Zmumu_ecm91'],
-				    'Ztautau':['p8_ee_Ztautau_ecm91'],
-   					'Zbb':['p8_ee_Zbb_ecm91'],
-				    'Zcc':['p8_ee_Zcc_ecm91'],
-				    'Zud':['p8_ee_Zud_ecm91'],
-				    'Zss':['p8_ee_Zss_ecm91'],
+					#'Zee':['p8_ee_Zee_ecm91'],
+   					#'Zmumu':['p8_ee_Zmumu_ecm91'],
+				    #'Ztautau':['p8_ee_Ztautau_ecm91'],
+   					#'Zbb':['p8_ee_Zbb_ecm91'],
+				    #'Zcc':['p8_ee_Zcc_ecm91'],
+				    #'Zud':['p8_ee_Zud_ecm91'],
+				    #'Zss':['p8_ee_Zss_ecm91'],
                     ## ##'emununu': ['emununu'],
                 },
                 }
