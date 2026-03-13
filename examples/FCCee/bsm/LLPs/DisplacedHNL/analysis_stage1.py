5440ed29c59eaaa5518d5f9058bcd4f810d78aa2
el-doardo <edoardospoto96.es@gmail.com>
2026-03-13 17:28:54 +0100

    Update analysis_stage1.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1.py
@@ -17,13 +17,13 @@ processList = {
 	#	Backgrounds centrally produced	#
 	## ## ## ## ## ## ## ## ## ## ## ## #
 	
-        'p8_ee_Zee_ecm91':{'fraction':1}, 		## ## secondo il paper lui non serve, ma sono curioso 	## ## fraction limit sperimentato: 1
-        'p8_ee_Zmumu_ecm91':{'fraction':0.25},		## ## fraction limit sperimentato: 0.25
-        'p8_ee_Ztautau_ecm91':{'fraction':0.25},	## ## fraction limit sperimentato: 0.25
-        'p8_ee_Zbb_ecm91':{'fraction':0.1},		## ## fraction limit sperimentato: 0.1
-        'p8_ee_Zcc_ecm91':{'fraction':0.1},		## ## fraction limit sperimentato: 0.1
-    	'p8_ee_Zud_ecm91':{'fraction':0.5},		## ## fraction limit sperimentato: 0.5 forse alzabile
-        'p8_ee_Zss_ecm91':{'fraction':0.5},		## ## fraction limit sperimentato: 0.5 forrse alzabile
+        'p8_ee_Zee_ecm91':{'fraction':0.01}, 		## ## secondo il paper lui non serve, ma sono curioso 	## ## fraction limit sperimentato: 1
+        'p8_ee_Zmumu_ecm91':{'fraction':0.001},		## ## fraction limit sperimentato: 0.25
+        'p8_ee_Ztautau_ecm91':{'fraction':0.001},	## ## fraction limit sperimentato: 0.25
+        'p8_ee_Zbb_ecm91':{'fraction':0.001},		## ## fraction limit sperimentato: 0.1
+        'p8_ee_Zcc_ecm91':{'fraction':0.001},		## ## fraction limit sperimentato: 0.1
+    	'p8_ee_Zud_ecm91':{'fraction':0.005},		## ## fraction limit sperimentato: 0.5 forse alzabile
+        'p8_ee_Zss_ecm91':{'fraction':0.005},		## ## fraction limit sperimentato: 0.5 forrse alzabile
 
 	## ## ## ## ## ## ## ## ## ## ## ## #
 	#	Backgrounds locally produced	#
