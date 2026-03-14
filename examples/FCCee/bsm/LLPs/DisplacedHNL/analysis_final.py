bf6b8458dafb2001dd74f378174094ef5fb22407
el-doardo <edoardospoto96.es@gmail.com>
2026-03-14 16:17:28 +0100

    Update analysis_final.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_final.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_final.py
@@ -24,8 +24,8 @@ processList = {
 
     #backgrounds
 	#'p8_ee_Zee_ecm91':{},
-    #'p8_ee_Zmumu_ecm91':{},
-    'p8_ee_Ztautau_ecm91':{},
+    'p8_ee_Zmumu_ecm91':{},
+    #'p8_ee_Ztautau_ecm91':{},
     #'p8_ee_Zbb_ecm91':{},
     #'p8_ee_Zcc_ecm91':{},
     #'p8_ee_Zud_ecm91':{},
