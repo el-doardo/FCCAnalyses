0c6f240c8483fad5fedc3ecfab2921f34d0ec87f
el-doardo <edoardospoto96.es@gmail.com>
2026-03-23 12:05:38 +0100

    Update analysis_stage1_significance.py

--- examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1_significance.py
+++ examples/FCCee/bsm/LLPs/DisplacedHNL/analysis_stage1_significance.py
@@ -544,20 +544,20 @@ processList_ = {
 #Production tag. This points to the yaml files for getting sample statistics
 #Mandatory when running over EDM4Hep centrally produced events
 #Comment out when running over privately produced events
-prodTag     = "FCCee/winter2023/IDEA/"
+#~prodTag     = "FCCee/winter2023/IDEA/"
 
 #Input directory
 #Comment out when running over centrally produced events
 #Mandatory when running over privately produced events
 #inputDir = "/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/IDEA/"
-#inputDir = "/eos/user/e/espoto/FCC_2jet_z_pole/FCCAnalysis/hadronized_signals"
+inputDir = "/eos/user/e/espoto/FCC_2jet_z_pole/FCCAnalysis/hadronized_signals"
 
 # additional/costom C++ functions, defined in header files (optional)
 includePaths = ["functions.h"]
 
 #Optional: output directory, default is local dir
 #outputDir = "output_stage1/"
-outputDir = "/eos/user/e/espoto/FCC_2jet_z_pole/FCCAnalysis/stage1_reco/"
+outputDir = "/eos/user/e/espoto/FCC_2jet_z_pole/FCCAnalysis/stage1_reco_significance/"
 
 ### necessary to run on HTCondor ###
 eosType = "eosuser"
